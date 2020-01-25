9c97b72cfd8c6a628e4ab461153006c5c375fd2f
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-01-25 18:25:13 +0800

    05 符号运算符

---
+++ Chapter05/05.符号运算符.c
@@ -0,0 +1,22 @@
+/**
+ * Date : 2020 - 01 - 25
+ * Author : Soler HO
+ * 
+ * Description : 符号运算符+ 、-
+ *  
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+    int num01 = -6;
+    int num02 = +4;
+
+    printf("减号（-）还可用于标明或改变一个值的代数符号");
+
+    /** 符号运算符+、-
+     * 
+     * 一元运算符（unary operator）：只需要一个运算对象。
+     */
+}
