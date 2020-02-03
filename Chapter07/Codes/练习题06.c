2cd74c931097b9d748de43312b044ee3aaf16937
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-02-03 14:32:53 +0800

    练习题06

---
+++ Chapter07/Codes/练习题06.c
@@ -0,0 +1,36 @@
+/**
+ * Date : 2020 - 02 - 03
+ * Author : Soler HO
+ * 
+ * Book :C Primer Plus
+ * 
+ * Description : 编写程序读取输入，读到#停止，输出ei出现的次数。
+ * 
+ * 逻辑运算符的应用
+ * 
+ */
+#include<stdio.h>
+#define STOP '#'
+
+int main(void)
+{
+    char ch;
+    int count = 0;
+    char first_char;
+
+    printf("请输入一些内容（输入#停止）：\n");
+
+    while((ch = getchar()) !=STOP)
+    {
+        if(ch=='e'){
+            first_char = ch; // 先让程序记住一个
+        }
+        if(ch=='i'&&first_char=='e') 
+        {
+            count++;
+        }
+    }
+    printf("内容中ei出现的次数是：%d \n",count);
+
+    return 0;
+}
